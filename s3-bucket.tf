cc5ba4e1e9ac412ee34f12339f0ac57f9f907e89
Ashwani Kumar <ashwanijee@gmail.com>
2022-06-21 07:56:12 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source  = "app.terraform.io/ashwanikumar-training/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "ashkum007-s3-bucket"
   acl    = "private"
 
   versioning = {
